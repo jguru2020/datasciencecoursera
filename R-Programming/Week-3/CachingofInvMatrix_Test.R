428a8b3d48de4aa3f303c6bb31cf657dc4c8c8a8
jguru2020 <63131280+jguru2020@users.noreply.github.com>
2020-04-16 14:28:12 -0500

    Test file for testing matrix caching

---
+++ R-Programming/Week-3/CachingofInvMatrix_Test.R
@@ -0,0 +1,15 @@
+load("CachingofInvMatrix.R")
+
+#2X2 matrxi test
+m <- matrix(c(0, 2, 1, 0), nrow = 2, ncol = 2, byrow = TRUE)
+m
+m1 = makeCacheMatrix(m)
+cacheSolve(m1)
+
+#3X3 matrix
+anotherMatrix  = matrix( c(5, 1, 0,
+                           3,-1, 2,
+                           4, 0,-1), nrow=3, byrow=TRUE)
+anotherMatrix
+am1 = makeCacheMatrix(anotherMatrix)
+cacheSolve(am1)
