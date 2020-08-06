163f264c25f25067c42c0700532861eb88e02bdd
jguru2020 <63131280+jguru2020@users.noreply.github.com>
2020-08-06 15:07:08 -0500

    Latest code added to repo

--- dataproducts/week4/ui.R
+++ dataproducts/week4/ui.R
@@ -4,8 +4,26 @@ library(dplyr)
 library(readr)
 library(purrr)
 
-#spend  = read_csv("https://jguru2020.github.io/data/Expenses.csv") 
-#cats = unique(spend$category)
-ui <-fluidPage(
-  plotlyOutput("pie"),uiOutput("back"))
-
+shinyUI(
+  navbarPage("Shiny Application",
+             tabPanel("Analysis",
+                      fluidPage(plotlyOutput("pie"),uiOutput("back"))
+             ),
+             tabPanel("Data Set",
+                      
+                      p("A data frame with 263 observations and 5 variables. 
+                        This data related to individuals expenses for the year 2019")
+             ),
+             tabPanel("How-To",
+                      h2("Usage of the application"),
+                      p("Drill-down the pie chart by clicking on individual categories. 
+                        Navigate back using the Back button ")
+                      
+             ),
+             tabPanel("Source Code",
+                      p(" Source code related to Shiny App (ui.R & server.R) uploaded to the following repository"),
+                      a("https://github.com/jguru2020/jguru2020.github.io/tree/master/dataproducts/week4")
+                      
+             )
+  )
+)
